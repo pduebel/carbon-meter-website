35154af8379db6c8b567ef3d91774670ca54074d
pduebel <pnduebel@gmail.com>
2021-05-15 21:43:08 +0100

    further testing

--- app/routes.py
+++ app/routes.py
@@ -66,7 +66,7 @@ def index():
 @app.route('/data-upload', methods=['POST'])
 def get_data():
     try:
-        r = request.get_json()
+        r = request.form
         return f'{r}', 200
     except:
         return 'Sad face :(', 400
