204084655ade890287a3ed3539caa8b22bf8a224
pduebel <pnduebel@gmail.com>
2021-05-22 17:20:40 +0100

    test kW upload

--- app/routes.py
+++ app/routes.py
@@ -81,14 +81,11 @@ def get_data():
 
 @app.route('/kW-upload', methods=['POST'])
 def get_kW():
-    try:
-        kW = request.form['kW']
-        id = 1
-        conn = sqlite3.connect('energy.db')
-        c = conn.cursor()
-        c.execute(f'REPLACE INTO kW (id, kW) VALUES ({id}, {kW})')
-        conn.commit()
-        conn.close()
-        return 'We did it!', 200
-    except:
-        return 'Sad face :(', 400 
+    kW = request.form['kW']
+    id = 1
+    conn = sqlite3.connect('energy.db')
+    c = conn.cursor()
+    c.execute(f'REPLACE INTO kW (id, kW) VALUES ({id}, {kW})')
+    conn.commit()
+    conn.close()
+    return 'We did it!', 200
