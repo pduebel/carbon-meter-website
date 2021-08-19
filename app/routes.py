26cc28eebfe9cf19da090523d7dae59b7e038a8b
pduebel <pnduebel@gmail.com>
2021-08-19 08:43:09 +0100

    add totals

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,7 @@ def index():
     conn = sqlite3.connect('energy.db')
     cur = conn.cursor()
     query_dict = {}
+    total_dict = {}
     days_dict = {'day': [1, 5],
                  'week': [7, 60],
                  'month': [30, 1440]}
@@ -40,57 +41,44 @@ def index():
             )
             GROUP BY timestamp_floor
         '''
+        total_dict[period] = f'''
+            SELECT
+              SUM(kWh),
+              SUM(carbon)
+            FROM energy
+            WHERE (
+                    (
+                        SELECT MAX(JULIANDAY(timestamp)) 
+                        FROM energy
+                    ) - JULIANDAY(timestamp)
+                ) <= {period_list[0]}
+        '''
     
     try:
         day_graph_df = pd.read_sql_query(query_dict['day'], con=conn)
         day_graph_df['timestamp_floor'] = pd.to_datetime(day_graph_df['timestamp_floor'])
+        day_totals = cur.execute(total_dict['day']).fetchone()
     except:
         day_graph_df = pd.DataFrame()
-    '''df['timestamp'] = pd.to_datetime(df['timestamp'])
-    
-    day_timestamp = df['timestamp'].max() - datetime.timedelta(hours=24)
-    day_df = df[df['timestamp'] >= day_timestamp].copy()
-    grouped_day_df = day_df.groupby(day_df['timestamp'].dt.floor('5min'))['kWh'].max().reset_index()
-    grouped_day_df['kWh_diff'] = grouped_day_df['kWh'].diff()
-    grouped_day_df['min_diff'] = grouped_day_df['timestamp'].diff().dt.total_seconds().div(60)
-    grouped_day_df['min_diff'] = grouped_day_df['min_diff'].fillna(1).astype(int)
-    grouped_day_df['diff'] = grouped_day_df['kWh_diff'] / grouped_day_df['min_diff']
-    
-    df.rename(columns={'kWh': 'diff'}, inplace=True)
-    day_graph_df = df[['timestamp', 'diff']]'''
+        day_totals = ['N/A', 'N/A']
     
     try:
         week_graph_df = pd.read_sql_query(query_dict['week'], con=conn)
         week_graph_df['timestamp_floor'] = pd.to_datetime(week_graph_df['timestamp_floor'])
+        week_totals = cur.execute(total_dict['week']).fetchone()
     except:
         week_graph_df = pd.DataFrame()
-    '''
-    week_timestamp = df['timestamp'].max() - datetime.timedelta(days=7)
-    week_df = df[df['timestamp'] >= week_timestamp].copy()
-    grouped_week_df = week_df.groupby(week_df['timestamp'].dt.floor('H'))['kWh'].max().reset_index()
-    grouped_week_df['kWh_diff'] = grouped_week_df['kWh'].diff()
-    grouped_week_df['hour_diff'] = grouped_week_df['timestamp'].diff().dt.total_seconds().div(60 * 60)
-    grouped_week_df['hour_diff'] = grouped_week_df['hour_diff'].fillna(1).astype(int)
-    grouped_week_df['diff'] = grouped_week_df['kWh_diff'] / grouped_week_df['hour_diff']
-    week_graph_df = grouped_week_df[['timestamp', 'diff']]'''
+        week_totals = ['N/A', 'N/A']
     
     try:
         month_graph_df = pd.read_sql_query(query_dict['month'], con=conn)
         month_graph_df['timestamp_floor'] = pd.to_datetime(month_graph_df['timestamp_floor'])
+        month_totals = cur.execute(total_dict['month']).fetchone()
     except:
         month_graph_df = pd.DataFrame()
-    '''month_timestamp = df['timestamp'].max() - datetime.timedelta(days=30)
-    month_df = df[df['timestamp'] >= month_timestamp].copy()
-    grouped_month_df = month_df.groupby(month_df['timestamp'].dt.floor('D'))['kWh'].max().reset_index()
-    grouped_month_df['kWh_diff'] = grouped_month_df['kWh'].diff()
-    grouped_month_df['day_diff'] = grouped_month_df['timestamp'].diff().dt.total_seconds().div(60 * 60 * 24)
-    grouped_month_df['day_diff'] = grouped_month_df['day_diff'].fillna(1).astype(int)
-    grouped_month_df['diff'] = grouped_month_df['kWh_diff'] / grouped_month_df['day_diff']
-    month_graph_df = grouped_month_df[['timestamp', 'diff']]'''
+        month_totals = ['N/A', 'N/A']
 
     description = [('timestamp_floor', 'datetime', 'Time stamp'),
-                   #('battery', 'number', 'Battery'),
-                   #('kWh', 'number', 'Total kWh'),
                    ('kWh', 'number', 'kWh'),
                    ('carbon', 'number', 'Carbon')]
 
@@ -118,11 +106,8 @@ def index():
     '''
     try:
         carbon_data =  cur.execute(carbon_query).fetchone()
-        carbon_intensity = carbon_data[0]
-        intensity_index = carbon_data[1]
     except:
-        carbon_intensity = 'N/A'
-        intensity_index = 'N/A'
+        carbon_data = ['N/A', 'N/A']
     
     try:
         kw_query = '''
@@ -141,8 +126,10 @@ def index():
                            week_json=week_json, 
                            month_json=month_json, 
                            kW=kW,
-                           carbon_intensity=carbon_intensity,
-                           intensity_index=intensity_index)
+                           carbon_data=carbon_data,
+                           day_totals=day_totals,
+                           week_totals=week_totals,
+                           month_totals=month_totals)
 
 @app.route('/data-upload', methods=['POST'])
 def get_data():
